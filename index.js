c112f4e9aaf285f610d5115a90b41995790270dd
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-28 13:36:19 +0000

    set up the server to listen on port 4000

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // implement your API here
 const express = require('express');
 const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get('*', handleDefaultRequest)
+
+function handleDefaultRequest(req, res) {
+    res.json('Its working')
+}
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log('port is listening on ' + (process.env.PORT || 4000))
+})
