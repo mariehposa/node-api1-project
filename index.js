56d0291703475ef28477cee1f868ab43beceed00
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-28 13:53:02 +0000

    create function to get all users

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 // implement your API here
 const express = require('express');
 const cors = require('cors');
+const db = require('./data/db')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
-app.get('*', handleDefaultRequest)
 app.get('/api/users', getAllUsers)
+app.get('*', handleDefaultRequest)
 
 function handleDefaultRequest(req, res) {
     res.json('Its working')
+}
+
+function getAllUsers (req, res) {
+    db.find()
+        .then(data => {
+            console.log(data)
+            res.json(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 app.listen(process.env.PORT || 4000, () => {
