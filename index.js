703d4457c38d929293f35a41682fee9cfd44d4fa
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-28 14:20:53 +0000

    create function to get user by Id

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json())
 
 app.get('/api/users', getAllUsers)
-app.get('*', handleDefaultRequest)
 app.get('/api/users/:id', getUserById)
+app.get('*', handleDefaultRequest)
 
 function handleDefaultRequest(req, res) {
     res.json('Its working')
@@ -20,16 +20,31 @@ function getAllUsers (req, res) {
     db.find()
         .then(data => {
             console.log(data)
-            res.status(500).json(data)
+            res.status(200).json(data)
         })
         .catch(err => {
             console.log(err)
-            res.json({
-                message:'The users information could not be retrieved',
-                err: "error 404"
+            res.status(500).json({
+                message:'The users information could not be retrieved'
             })
         })
 }
+
+function getUserById(req, res) {
+    const { id } = req.params;
+    db.findById(id)
+        .then(data => {
+            console.log(data)
+            res.status(200).json(data)
+        })
+        .catch(error => {
+            res.status(404).json({
+                message:'The user with the specified ID does not exist.'
+            })
+        })
+}
+
+
 
 app.listen(process.env.PORT || 4000, () => {
     console.log('port is listening on ' + (process.env.PORT || 4000))
